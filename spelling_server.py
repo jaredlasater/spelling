52ac7a69565a3333f190bea4a8083f0f4eaf87f4
jaredlasater <jaredlasater@hotmail.com>
2025-03-27 16:23:33 +0000

    Add Create Quiz functionality and improve dialog layout

--- spelling_server.py
+++ spelling_server.py
@@ -1,407 +1,108 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 import os
-
+import requests
 
 app = Flask(__name__)
 
 spelling_list = {
-  "unit": {
-    "id": 28,
-    "name": "2.8",
-    "description": "Words with Long Vowels and Fun Endings",
-    "spelling-words": [
-      {
-        "id": 1,
-        "word": "avoided",
-        "sentence": "She avoided the puddle so her shoes stayed dry.",
-        "state": ""
-      },
-      {
-        "id": 2,
-        "word": "daily",
-        "sentence": "He brushes his teeth daily to keep them clean.",
-        "state": ""
-      },
-      {
-        "id": 3,
-        "word": "defeat",
-        "sentence": "The team worked hard to defeat their opponents.",
-        "state": ""
-      },
-      {
-        "id": 4,
-        "word": "enjoyed",
-        "sentence": "We enjoyed playing outside on the sunny day.",
-        "state": ""
-      },
-      {
-        "id": 5,
-        "word": "explode",
-        "sentence": "The balloon will explode if you blow it up too much.",
-        "state": ""
-      },
-      {
-        "id": 6,
-        "word": "gained",
-        "sentence": "She gained a new friend at school today.",
-        "state": ""
-      },
-      {
-        "id": 7,
-        "word": "getaway",
-        "sentence": "The cat made a quick getaway from the dog.",
-        "state": ""
-      },
-      {
-        "id": 8,
-        "word": "idea",
-        "sentence": "He had a great idea for a fun game.",
-        "state": ""
-      },
-      {
-        "id": 9,
-        "word": "outgrow",
-        "sentence": "I outgrow my shoes because my feet get bigger.",
-        "state": ""
-      },
-      {
-        "id": 10,
-        "word": "phrase",
-        "sentence": "‘Good job’ is a nice phrase to say.",
-        "state": ""
-      },
-      {
-        "id": 11,
-        "word": "produce",
-        "sentence": "Farmers produce yummy fruits and vegetables.",
-        "state": ""
-      },
-      {
-        "id": 12,
-        "word": "revenge",
-        "sentence": "He didn’t want revenge; he just wanted to play fair.",
-        "state": ""
-      },
-      {
-        "id": 13,
-        "word": "toaster",
-        "sentence": "Mom put bread in the toaster for breakfast.",
-        "state": ""
-      },
-      {
-        "id": 14,
-        "word": "unseen",
-        "sentence": "The kitten hid in an unseen spot under the bed.",
-        "state": ""
-      },
-      {
-        "id": 15,
-        "word": "yogurt",
-        "sentence": "I like to eat yogurt with fruit for a snack.",
-        "state": ""
-      }
-    ]
-  }
+    "unit": {
+        "id": 28,
+        "name": "2.8",
+        "description": "Words with Long Vowels and Fun Endings",
+        "spelling-words": [
+            {
+                "id": 1,
+                "word": "avoided",
+                "sentence": "She avoided the puddle so her shoes stayed dry.",
+                "state": ""
+            },
+            {
+                "id": 2,
+                "word": "daily",
+                "sentence": "He brushes his teeth daily to keep them clean.",
+                "state": ""
+            },
+            {
+                "id": 3,
+                "word": "defeat",
+                "sentence": "The team worked hard to defeat their opponents.",
+                "state": ""
+            },
+            {
+                "id": 4,
+                "word": "enjoyed",
+                "sentence": "We enjoyed playing outside on the sunny day.",
+                "state": ""
+            },
+            {
+                "id": 5,
+                "word": "explode",
+                "sentence": "The balloon will explode if you blow it up too much.",
+                "state": ""
+            },
+            {
+                "id": 6,
+                "word": "gained",
+                "sentence": "She gained a new friend at school today.",
+                "state": ""
+            },
+            {
+                "id": 7,
+                "word": "getaway",
+                "sentence": "The cat made a quick getaway from the dog.",
+                "state": ""
+            },
+            {
+                "id": 8,
+                "word": "idea",
+                "sentence": "He had a great idea for a fun game.",
+                "state": ""
+            },
+            {
+                "id": 9,
+                "word": "outgrow",
+                "sentence": "I outgrow my shoes because my feet get bigger.",
+                "state": ""
+            },
+            {
+                "id": 10,
+                "word": "phrase",
+                "sentence": "‘Good job’ is a nice phrase to say.",
+                "state": ""
+            },
+            {
+                "id": 11,
+                "word": "produce",
+                "sentence": "Farmers produce yummy fruits and vegetables.",
+                "state": ""
+            },
+            {
+                "id": 12,
+                "word": "revenge",
+                "sentence": "He didn’t want revenge; he just wanted to play fair.",
+                "state": ""
+            },
+            {
+                "id": 13,
+                "word": "toaster",
+                "sentence": "Mom put bread in the toaster for breakfast.",
+                "state": ""
+            },
+            {
+                "id": 14,
+                "word": "unseen",
+                "sentence": "The kitten hid in an unseen spot under the bed.",
+                "state": ""
+            },
+            {
+                "id": 15,
+                "word": "yogurt",
+                "sentence": "I like to eat yogurt with fruit for a snack.",
+                "state": ""
+            }
+        ]
+    }
 }
-
-
-
-
-# spelling_list = {
-#   "unit": {
-#     "id": 27,
-#     "name": "2.7",
-#     "description": "Vowel Teams OW & OA",
-#     "spelling-words": [
-#       {
-#         "id": 1,
-#         "word": "arrow",
-#         "sentence": "The arrow pointed to the treasure on the map.",
-#         "state": ""
-#       },
-#       {
-#         "id": 2,
-#         "word": "boast",
-#         "sentence": "My friend likes to boast about his fast bike.",
-#         "state": ""
-#       },
-#       {
-#         "id": 3,
-#         "word": "cloak",
-#         "sentence": "The superhero wore a red cloak that flapped in the wind.",
-#         "state": ""
-#       },
-#       {
-#         "id": 4,
-#         "word": "floated",
-#         "sentence": "My boat floated on the water at the lake.",
-#         "state": ""
-#       },
-#       {
-#         "id": 5,
-#         "word": "follow",
-#         "sentence": "I follow my teacher to the lunchroom every day.",
-#         "state": ""
-#       },
-#       {
-#         "id": 6,
-#         "word": "groan",
-#         "sentence": "I heard a groan when my brother stubbed his toe.",
-#         "state": ""
-#       },
-#       {
-#         "id": 7,
-#         "word": "oatmeal",
-#         "sentence": "Mom made warm oatmeal with honey for breakfast.",
-#         "state": ""
-#       },
-#       {
-#         "id": 8,
-#         "word": "outgrow",
-#         "sentence": "I outgrow my shoes because my feet get bigger.",
-#         "state": ""
-#       },
-#       {
-#         "id": 9,
-#         "word": "pillow",
-#         "sentence": "My soft pillow helps me sleep at night.",
-#         "state": ""
-#       },
-#       {
-#         "id": 10,
-#         "word": "raincoat",
-#         "sentence": "I wear my raincoat when it rains on my way to school.",
-#         "state": ""
-#       },
-#       {
-#         "id": 11,
-#         "word": "shadow",
-#         "sentence": "My shadow follows me when I walk in the sun.",
-#         "state": ""
-#       },
-#       {
-#         "id": 12,
-#         "word": "soapy",
-#         "sentence": "My hands got soapy when I washed the dishes.",
-#         "state": ""
-#       },
-#       {
-#         "id": 13,
-#         "word": "throwing",
-#         "sentence": "We had fun throwing a ball at the park.",
-#         "state": ""
-#       },
-#       {
-#         "id": 14,
-#         "word": "toaster",
-#         "sentence": "The toaster popped up my bread for breakfast.",
-#         "state": ""
-#       },
-#       {
-#         "id": 15,
-#         "word": "yellow",
-#         "sentence": "The yellow sun shines bright in the sky.",
-#         "state": ""
-#       }
-#     ]
-#   }
-# }
-# spelling_list = {
-
-#     "unit": {
-#         "id": 2,
-#         "name": "2.6",
-#         "description": "Vowel Teams EA & EE",
-#         "spelling-words": [{
-#         "id": 1,
-#         "word": "defeat",
-#         "sentence": "We can defeat the other team if we try our best.",
-#         "state": ""
-#       },
-#       {
-#         "id": 2,
-#         "word": "unreal",
-#         "sentence": "The dragon in my book looks unreal but so cool!",
-#         "state": ""
-#       },
-#       {
-#         "id": 3,
-#         "word": "sixteen",
-#         "sentence": "I counted sixteen balloons at the party.",
-#         "state": ""
-#       },
-#       {
-#         "id": 4,
-#         "word": "mistreat",
-#         "sentence": "It’s not nice to mistreat your toys by throwing them.",
-#         "state": ""
-#       },
-#       {
-#         "id": 5,
-#         "word": "degree",
-#         "sentence": "My teacher has a degree to teach us fun things.",
-#         "state": ""
-#       },
-#       {
-#         "id": 6,
-#         "word": "unseen",
-#         "sentence": "The bug was unseen because it hid under a leaf.",
-#         "state": ""
-#       },
-#       {
-#         "id": 7,
-#         "word": "wheat",
-#         "sentence": "Bread is made from wheat that grows in fields.",
-#         "state": ""
-#       },
-#       {
-#         "id": 8,
-#         "word": "disagree",
-#         "sentence": "I disagree with my friend about the best color.",
-#         "state": ""
-#       },
-#       {
-#         "id": 9,
-#         "word": "agreed",
-#         "sentence": "We agreed to play tag at recess today.",
-#         "state": ""
-#       },
-#       {
-#         "id": 10,
-#         "word": "ordeal",
-#         "sentence": "Cleaning my messy room was a big ordeal!",
-#         "state": ""
-#       },
-#       {
-#         "id": 11,
-#         "word": "spree",
-#         "sentence": "We went on a fun spree picking out candy.",
-#         "state": ""
-#       },
-#       {
-#         "id": 12,
-#         "word": "breed",
-#         "sentence": "The farmer wants to breed cows on his farm.",
-#         "state": ""
-#       },
-#       {
-#         "id": 13,
-#         "word": "squeal",
-#         "sentence": "I heard a squeal when my sister saw a mouse.",
-#         "state": ""
-#       },
-#       {
-#         "id": 14,
-#         "word": "sheen",
-#         "sentence": "My new shoes have a shiny sheen I like.",
-#         "state": ""
-#       },
-#       {
-#         "id": 15,
-#         "word": "indeed",
-#         "sentence": "It is indeed a sunny day to play outside.",
-#         "state": ""
-#       }
-#         ]
-#     }
-
-# }
-# spelling_list = {
-#   "unit": {
-#     "id": 1,
-#     "name": "2.1",
-#     "description": "oi, oy", 
-#     "spelling-words": [
-#       {
-#         "id": 1,
-#         "word": "employ",
-#         "sentence": "My mom works at a store where they employ many people.",
-#         "state": ""
-#       }
-#       ,
-#       {
-#         "id": 2,
-#         "word": "loyal",
-#         "sentence": "My dog is very loyal and always stays by my side.",
-#         "state": ""
-#       }
-#       ,
-#       {
-#         "id": 3,
-#         "word": "avoided",
-#         "sentence": "The cat avoided the puddle on the sidewalk.",
-#         "state": ""
-#       },
-#       {
-#         "id": 4,
-#         "word": "enjoyed",
-#         "sentence": "We enjoyed playing games at the birthday party.",
-#         "state": ""
-#       },
-#       {
-#         "id": 5,
-#         "word": "noise",
-#         "sentence": "The loud noise of the fire truck woke me up.",
-#         "state": ""
-#       },
-#       {
-#         "id": 6,
-#         "word": "broil",
-#         "sentence": "My dad will broil the chicken for dinner.",
-#         "state": ""
-#       },
-#       {
-#         "id": 7,
-#         "word": "hoist",
-#         "sentence": "They will hoist the flag up the flagpole.",
-#         "state": ""
-#       },
-#       {
-#         "id": 8,
-#         "word": "oily",
-#         "sentence": "The pizza was oily, but it tasted good.",
-#         "state": ""
-#       },
-#       {
-#         "id": 9,
-#         "word": "choice",
-#         "sentence": "I had a choice between ice cream and cake.",
-#         "state": ""
-#       },
-#       {
-#         "id": 10,
-#         "word": "invoice",
-#         "sentence": "My dad paid the invoice for the new light.",
-#         "state": ""
-#       },
-#       {
-#         "id": 11,
-#         "word": "rejoin",
-#         "sentence": "I will rejoin my friends at the park after school.",
-#         "state": ""
-#       },
-#       {
-#         "id": 12,
-#         "word": "decoy",
-#         "sentence": "The hunter used a decoy to attract the ducks.",
-#         "state": ""
-#       },
-#       {
-#         "id": 13,
-#         "word": "joyful",
-#         "sentence": "The children were joyful when they saw the clown.",
-#         "state": ""
-#       },
-#       {
-#         "id": 14,
-#         "word": "spoiled",
-#         "sentence": "The milk got spoiled because it was left out.",
-#         "state": ""
-#       }
-#     ]
-#   }
-# }
-
 
 @app.route('/')
 def index():
@@ -409,8 +110,84 @@ def index():
 
 @app.route('/get_quiz_data')
 def get_quiz_data():
-    return jsonify(spelling_list) #send the spelling list as json
+    return jsonify(spelling_list)  # Send the spelling list as JSON
 
+@app.route('/generate_sentences', methods=['POST'])
+def generate_sentences():
+    print("Received request for /generate_sentences")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True) # Ensure this line is present
+    data = request.json
+    words = data.get('words', [])
+    unit_id = data.get('unit_id', 1)  # Default to 1 if not provided
+    unit_name = data.get('unit_name', "Generated Unit")  # Default to "Generated Unit" if not provided
+    unit_description = data.get('unit_description', "Generated sentences for the provided words")  # Default description
+
+    if not words:
+        return jsonify({"error": "No words provided"}), 400
+
+    # Construct the prompt
+    prompt = (
+        f"Generate a JSON object with the following structure:\n"
+        f"{{\n"
+        f"  \"unit\": {{\n"
+        f"    \"id\": {unit_id},\n"
+        f"    \"name\": \"{unit_name}\",\n"
+        f"    \"description\": \"{unit_description}\",\n"
+        f"    \"spelling-words\": [\n"
+        f"      {{\n"
+        f"        \"id\": <unique_id>,\n"
+        f"        \"word\": <word>,\n"
+        f"        \"sentence\": <sentence appropriate for a second grader>,\n"
+        f"        \"state\": \"\"\n"
+        f"      }}\n"
+        f"    ]\n"
+        f"  }}\n"
+        f"}}\n\n"
+        f"Generate this JSON object for the following words: {', '.join(words)}. Only return the JSON object in the response."
+    )
+
+    # Construct the payload for Grok's API
+    payload = {
+        "messages": [
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        "model": "grok-2-latest",
+        "stream": False,
+        "temperature": 0
+    }
+
+    # Output the payload to the console
+    print("Payload being sent to Grok API:", payload)
+
+    try:
+        # Make a POST request to Grok's API
+        response = requests.post(
+            "https://api.x.ai/v1/chat/completions",  # Grok's API endpoint
+            headers={
+                "Authorization": f"Bearer {"xai-"}",  # Replace with your actual API key
+                "Content-Type": "application/json"
+            },
+            json=payload
+        )
+        response.raise_for_status()  # Raise an error for HTTP errors
+
+        # Extract the content from the response
+        grok_response = response.json()
+        content = grok_response["choices"][0]["message"]["content"]
+
+        # Parse the JSON content (removing the code block markers if present)
+        if content.startswith("```json"):
+            content = content[7:-3].strip()  # Remove the ```json and ``` markers
+
+        # Return the parsed JSON content
+        return jsonify(content)
+    except requests.exceptions.RequestException as e:
+        return jsonify({"error": f"Request failed: {e}"}), 500
+    except Exception as e:
+        return jsonify({"error": f"Unexpected error: {e}"}), 500
+
+if __name__ == '__main__': 
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
